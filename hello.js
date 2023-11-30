01035e1822284068ffeed5b7eb9455b11a633467
oussamaelfouah <oelfouah@gmail.com>
2023-11-30 16:48:27 +0100

    hello

---
+++ hello.js
@@ -0,0 +1,14 @@
+const panels_dom = document.getElementsByClassName('panel');
+const panels = [...panels_dom];
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('active');
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    })
+}
